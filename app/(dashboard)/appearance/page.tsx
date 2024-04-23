aa1896e1a2df90cb3ce861744a76c3cbd92c2659
jawad-nsu <jawadurrahim1997@gmail.com>
2024-04-23 11:57:12 -0400

    created form: profile_title + Bio

--- app/(dashboard)/appearance/page.tsx
+++ app/(dashboard)/appearance/page.tsx
@@ -1,26 +1,42 @@
+// Custom components
 import MobilePreview from '@/components/admin/mobile-preview';
+import TitleBioForm from '@/components/appearance/title-bio-form';
+import UpgradeCta from '@/components/appearance/upgrade-cta';
+
+// Shadcn-ui
 import { Button } from '@/components/ui/button';
 
 const Appearance = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-7'>
-      <div className='col-span-5 p-6 bg-inherit'>
+      <div className='col-span-5 p-6 bg-inherit gap-y-0'>
+        {/* Profile */}
         <div className='max-w-2xl mx-auto px-4 '>
           <h3 className='font-bold text-lg'>Profile</h3>
-          <div className='md:w-9/12 py-6 rounded-3xl bg-white'>
-            <div className='flex space-x-6 p-3 bg-purple-50'>
+          <div className='md:w-9/12 py-6 rounded-3xl bg-white space-y-8'>
+            {/* Profile Image */}
+            <div className='flex space-x-6 p-5'>
               <img
                 src='/navbar/avatar1.jpg'
-                className='h-[6rem] w-[6rem] rounded-[50%]'
+                className='h-[6rem] w-[6rem] rounded-[50%] items-center'
               />
               <div className='flex flex-col flex-1 space-y-2'>
-                <Button className='py-2 bg-purple-700 rounded-full font-bold hover:bg-purple-800'>
+                <Button className='py-6 bg-purple-700 rounded-full md:text-lg font-bold hover:bg-purple-800'>
                   Pick an image
                 </Button>
-                <Button className='py-2 bg-white border- rounded-full font-bold hover:bg-purple-800'></Button>
+                <Button className='py-6 bg-white border-2 border-gray-100 rounded-full md:text-lg text-black font-bold hover:bg-gray-100 '>
+                  Remove
+                </Button>
               </div>
             </div>
+            {/* Title & Bio */}
+            <TitleBioForm />
           </div>
+        </div>
+
+        {/* Upgrade CTA */}
+        <div className='max-w-2xl mx-auto px-4 bg-lime-300'>
+          <UpgradeCta />
         </div>
       </div>
 
---
+++ components/appearance/title-bio-form.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+import { useForm } from 'react-hook-form';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+
+const formSchema = z.object({
+  profile_title: z.string().min(2).max(50),
+  bio: z.string().min(2).max(80),
+});
+
+const TitleBioForm = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      profile_title: 'Jawad Rahim',
+      bio: 'IndieHacker in making',
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <div className='p-5'>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='space-y-8 flex flex-col'
+        >
+          <FormField
+            control={form.control}
+            name='profile_title'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className='text-gray-500'>Profile Title</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder=''
+                    className='rounded-xl bg-gray-100 border-0'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='bio'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className='text-gray-500'>Bio</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder=''
+                    className='bg-gray-100 border-0'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className='bg-purple-700 rounded-full' type='submit'>
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default TitleBioForm;
