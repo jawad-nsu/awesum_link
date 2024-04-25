b9fc3b4b5a72722121427c3fdd02e6cda4eb911c
jawad-nsu <jawadurrahim1997@gmail.com>
2024-04-25 11:16:44 -0400

    import theme component

--- app/(dashboard)/appearance/page.tsx
+++ app/(dashboard)/appearance/page.tsx
@@ -1,5 +1,6 @@
 // Custom components
 import MobilePreview from '@/components/admin/mobile-preview';
+import Themes from '@/components/appearance/themes';
 import TitleBioForm from '@/components/appearance/title-bio-form';
 import UpgradeCta from '@/components/appearance/upgrade-cta';
 
@@ -11,30 +12,34 @@ const Appearance = () => {
     <div className='grid grid-cols-1 md:grid-cols-7'>
       <div className='col-span-5 p-6 bg-inherit'>
         {/* Profile */}
-        <div className='max-w-4xl mx-auto px-4 space-y-4'>
-          <h3 className='font-bold text-xl'>Profile</h3>
-          <div className='md:w-9/12 py-6 rounded-3xl bg-white space-y-8'>
-            {/* Profile Image */}
-            <div className='flex space-x-6 p-5'>
-              <img
-                src='/navbar/avatar1.jpg'
-                className='h-[6rem] w-[6rem] rounded-[50%] items-center'
-              />
-              <div className='flex flex-col flex-1 space-y-2'>
-                <Button className='py-6 bg-purple-700 rounded-full md:text-lg font-bold hover:bg-purple-800'>
-                  Pick an image
-                </Button>
-                <Button className='py-6 bg-white border-2 border-gray-100 rounded-full md:text-lg text-black font-bold hover:bg-gray-100 '>
-                  Remove
-                </Button>
+        <div className='max-w-4xl mx-auto px-4 space-y-14'>
+          <div className='space-y-4'>
+            <h3 className='font-bold text-xl'>Profile</h3>
+            <div className='md:w-9/12 py-6 rounded-3xl bg-white space-y-8'>
+              {/* Profile Image */}
+              <div className='flex space-x-6 p-5'>
+                <img
+                  src='/navbar/avatar1.jpg'
+                  className='h-[6rem] w-[6rem] rounded-[50%] items-center'
+                />
+                <div className='flex flex-col flex-1 space-y-2'>
+                  <Button className='py-6 bg-purple-700 rounded-full md:text-lg font-bold hover:bg-purple-800'>
+                    Pick an image
+                  </Button>
+                  <Button className='py-6 bg-white border-2 border-gray-100 rounded-full md:text-lg text-black font-bold hover:bg-gray-100 '>
+                    Remove
+                  </Button>
+                </div>
               </div>
+              {/* Title & Bio */}
+              <TitleBioForm />
             </div>
-            {/* Title & Bio */}
-            <TitleBioForm />
           </div>
-
           {/* Upgrade CTA */}
           <UpgradeCta />
+
+          {/* Themes */}
+          <Themes />
         </div>
       </div>
 
