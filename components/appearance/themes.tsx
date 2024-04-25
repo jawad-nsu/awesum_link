5c40221f6dc3b952b1639d5c25b49befbf26835d
jawad-nsu <jawadurrahim1997@gmail.com>
2024-04-25 11:17:11 -0400

    themes component

---
+++ components/appearance/themes.tsx
@@ -0,0 +1,19 @@
+const Themes = () => {
+  return (
+    <div className='space-y-4'>
+      <h3 className='font-bold text-xl'>Themes</h3>
+      <div className='md:w-9/12 py-6 rounded-3xl bg-white grid grid-cols-4 gap-4'>
+        Hello
+      </div>
+    </div>
+  );
+};
+
+export default Themes;
+
+const themes = [
+  {
+    background: '#f7f7f7',
+    button: '#000000',
+  },
+];
