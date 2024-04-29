50733b8f67a50c698758905a7edf00f73c177e6b
jawad-nsu <jawadurrahim1997@gmail.com>
2024-04-28 21:16:54 -0400

    upgrade to remove logo CTA component complete

--- components/appearance/logo-upgrade-cta.tsx
+++ components/appearance/logo-upgrade-cta.tsx
@@ -1,22 +1,28 @@
 import Image from 'next/image';
 
+// icons
 import { Check, LockKeyhole, Zap } from 'lucide-react';
-import { Button } from '../ui/button';
 import { SiLinktree } from 'react-icons/si';
+
+// shadcn
+import { Button } from '../ui/button';
+
+// utility
+import { cn } from './../../lib/utils';
 
 const LogoUpgradeCta = () => {
   return (
-    <div className='md:w-9/12 p-5 rounded-3xl bg-white'>
+    <div className='md:w-9/12 p-5 rounded-3xl bg-white space-y-6'>
       <div className='flex justify-between'>
         <h3>Hide the Linktree logo</h3>
-        <Button className='px-2 py-0 leading-1 font-bold text-base'>
+        <button className='px-2 font-bold text-sm bg-black text-white flex items-center rounded-md'>
           Upgrade
           <LockKeyhole size={14} className='ml-1' />
-        </Button>
+        </button>
       </div>
       <div className='flex items-center'>
-        <h1 className='font-bold text-2xl'>Linktree</h1>
-        <SiLinktree size={20} className='' />
+        <h1 className='font-extrabold text-2xl'>Linktree</h1>
+        <SiLinktree size={22} className='text-green-500' />
       </div>
     </div>
   );
