8d311e5e74d4229ff0c0f6b785e8f15e022f35aa
jawad-nsu <jawadurrahim1997@gmail.com>
2024-05-09 12:13:50 -0400

    add icon components and the appropriate size

---
+++ lib/const.tsx
@@ -0,0 +1,65 @@
+import {
+  FileText,
+  Palette,
+  Pencil,
+  MonitorPlay,
+  Milk,
+  Pizza,
+  Scale,
+  Apple,
+  Flag,
+  LayoutGrid,
+  Monitor,
+  Plane,
+} from 'lucide-react';
+
+export const categories = [
+  {
+    name: 'Business',
+    icon: <FileText size={14} />,
+  },
+  {
+    name: 'Creative',
+    icon: <Palette size={14} />,
+  },
+  {
+    name: 'Education',
+    icon: <Pencil size={14} />,
+  },
+  {
+    name: 'Entertainment',
+    icon: <MonitorPlay size={14} />,
+  },
+  {
+    name: 'Fashion & Beauty',
+    icon: <Milk size={14} />,
+  },
+  {
+    name: 'Food & Beverage',
+    icon: <Pizza size={14} />,
+  },
+  {
+    name: 'Government & Politics',
+    icon: <Scale size={15} />,
+  },
+  {
+    name: 'Health & Wellness',
+    icon: <Apple size={14} />,
+  },
+  {
+    name: 'Non-profit',
+    icon: <Flag size={14} />,
+  },
+  {
+    name: 'Other',
+    icon: <LayoutGrid size={14} />,
+  },
+  {
+    name: 'Tech',
+    icon: <Monitor size={14} />,
+  },
+  {
+    name: 'Travel & Tourism',
+    icon: <Plane size={14} />,
+  },
+];
