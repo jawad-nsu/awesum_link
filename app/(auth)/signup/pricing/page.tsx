2065a9a59d0a451ce630f4d8884b5f2e375aebfc
jawad-nsu <jawadurrahim1997@gmail.com>
2024-05-09 21:41:04 -0400

    reusable pricing card

--- app/(auth)/signup/pricing/page.tsx
+++ app/(auth)/signup/pricing/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState, useEffect } from 'react';
 
+import { pricings } from '@/lib/const';
+
 import { Check } from 'lucide-react';
 import { SiLinktree } from 'react-icons/si';
 
@@ -28,29 +30,34 @@ const Pricing = () => {
 
       {/* Heading */}
       <div className='mt-20'>
-        <h1 className=' font-black text-5xl tracking-tight text-center '>
+        <h1 className='font-black text-5xl tracking-tight text-center'>
           Find the plan for you
         </h1>
         <h6 className='mt-2 text-center'>You can change at anytime</h6>
       </div>
 
       {/* Plans */}
-      <div className='mt-12'>
-        <ul className='flex justify-around'>
-          <li>
-            <Card title={'Starter'} price={0} features={'feature'} />
-          </li>
-          <li>
-            <span>Starter</span>
-          </li>
-          <li>
-            <span>Pro</span>
-          </li>
-          <li>
-            <span>Premium</span>
-          </li>
-        </ul>
+      {/* <div className='mt-12 '> */}
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-12 '>
+        {pricings.map(
+          ({
+            title,
+            subHeading,
+            features,
+            price,
+            billingCycle,
+          }: {
+            title: string;
+            subHeading: string;
+            features: string[];
+            price: string;
+            billingCycle: string;
+          }) => (
+            <Card {...{ title, subHeading, features, price, billingCycle }} />
+          )
+        )}
       </div>
+      {/* </div> */}
     </div>
   );
 };
@@ -59,38 +66,54 @@ export default Pricing;
 
 const Card = ({
   title,
-  price,
+  subHeading,
   features,
+  price,
+  billingCycle,
 }: {
   title: string;
-  price: number;
-  features: string;
+  subHeading: string;
+  features: string[];
+  price: string;
+  billingCycle: string;
 }) => {
   return (
-    <div className='border rounded-2xl'>
+    <div className='border rounded-2xl max-w-[20rem]'>
       <p className='pt-8 pb-4 px-4'>
         <div className='flex justify-between'>
           <label
             htmlFor='pricing-starter'
             className='font-extrabold text-xl leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
           >
-            Free
+            {title}
           </label>
-          <Checkbox id='pricing-starter' />
+          <Checkbox className='border active:bg-white' id='pricing-starter' />
         </div>
-        <h6 className='text-xs font-bold tracking-tights mt-2'>
-          For your personal Linktree
-        </h6>
+        <h6 className='text-xs font-bold tracking-tights mt-3'>{subHeading}</h6>
       </p>
-      <div className='border-t py-4 px-4'>
-        <h6 className='font-bold tracking-tight text-sm'>
-          For your personal Linktree
-        </h6>
-        <ul>
-          <li className='flex items-center space-x-2'>
-            <Check size={18} /> <span>Unlimited links</span>
-          </li>
-        </ul>
+      <div className='flex flex-col border-t py-4 px-4'>
+        <div>
+          <h6 className='font-bold tracking-tight text-sm'>{subHeading}</h6>
+          {/* Features */}
+          <ul className='space-y-2'>
+            {features.map((feature: string) => (
+              <li className='flex items-start space-x-2'>
+                <Check
+                  size={18}
+                  className='flex-none mt-[0.25rem]'
+                  color='#8b5cf6'
+                />{' '}
+                <span>{feature}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* footer - price */}
+        <div className='place-items-end'>
+          <h6 className='font-extrabold text-xl'>{price}</h6>
+          <h5 className='font-light'>{billingCycle}</h5>
+        </div>
       </div>
     </div>
   );
